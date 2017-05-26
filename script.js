8f54378dff7515baf84651c830c9c55f0fd9da89
Michael Zandstra <michael@tunnel.ninja>
2017-05-26 14:21:19 -0500

    add js builds

---
+++ script.js
@@ -0,0 +1,8 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=function(){return console.log("setting up nav...")};
+
+},{}],2:[function(require,module,exports){
+"use strict";function _interopRequireDefault(e){return e&&e.__esModule?e:{default:e}}var _nav=require("./nav"),_nav2=_interopRequireDefault(_nav);(0,_nav2.default)();
+
+},{"./nav":1}]},{},[2])
+//# sourceMappingURL=script.js.map
