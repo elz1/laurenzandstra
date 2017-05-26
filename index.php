f9b593986f5441bbae22a90d80da55f9b988fc9d
Michael Zandstra <michael@tunnel.ninja>
2017-05-26 16:45:01 -0500

    add containers

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php get_header(); ?>
 
+<main>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
   <h1><?php the_title() ?></h1>
   <div class="content"><?php the_content(); ?></div>
 <?php endwhile; else : ?>
   <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
 <?php endif; ?>
+</main>
 
 <?php get_footer(); ?>
