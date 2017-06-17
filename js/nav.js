70ce213712e6d5b543bb1fa07298214e56e91302
Michael Zandstra <michael@tunnel.ninja>
2017-06-17 16:32:55 -0500

    add menu toggle js

--- js/nav.js
+++ js/nav.js
@@ -1,1 +1,23 @@
-export default () => console.log('setting up nav...');
+import debug from 'debug';
+const d = debug('nav');
+const $ = sel => document.querySelector(sel);
+
+export default function setupNav() {
+
+  // log
+  d('setting up');
+
+  // dom node references
+  const menu = $('nav .links');
+  const toggle = $('nav .toggle');
+
+  // helper functions
+  const toggleMenu = () => menu.classList.toggle('active');
+
+  // add click listener to toggle
+  toggle.addEventListener('click', () => {
+    d('toggle clicked');
+    toggleMenu();
+  });
+
+}
